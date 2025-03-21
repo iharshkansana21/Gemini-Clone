373f837314a1b5c4859f71e652125f35edfda51c
iharshkansana21 <iharshkansana@gmail.com>
2025-03-21 21:25:19 +0530

    first commit

---
+++ src/config/gemini.js
@@ -0,0 +1,36 @@
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai"
+
+const apiKey = "AIzaSyBlh2qGC95P6zZi3xV5s0b0MBPsuy4n_08";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  const response = result.response.text();
+  console.log(result.response.text());
+  return response;
+}
+
+export default run;
+
